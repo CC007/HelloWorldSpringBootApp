663bb4820df5a08536355f5239c2be7e7b7df94b
Rik Schaaf <schaaf.r@tkppensioen.nl>
2023-10-06 13:37:13 +0200

    Simple HelloWorld application using Spring Boot

---
+++ src/test/java/com/github/cc007/oktetodemo/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cc007.oktetodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
